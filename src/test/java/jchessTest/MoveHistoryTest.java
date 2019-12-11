afc7f07857be0c01cb6c67a760771c0d37152a70
MariamRiaz <mariamriaz51@gmail.com>
2019-12-11 20:09:00 +0100

    minor change

--- src/test/java/jchessTest/MoveHistoryTest.java
+++ src/test/java/jchessTest/MoveHistoryTest.java
@@ -1,16 +1,12 @@
 package jchessTest;
 
-import org.junit.Assert;
-import org.junit.Test;
 import jchess.Game;
-import jchess.Settings;
 import jchess.pieces.MoveHistory;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class MoveHistoryTest {
     Game game;
@@ -21,11 +17,6 @@ public class MoveHistoryTest {
         moveHistory = new MoveHistory(game);
     }
     @Test
-    public void getValueAt() {
-/*        when(moveHistory.getValueAt(1,1).thenReturn("a2-a3"));
-        assertEquals("a2-a3", moveHistory.getValueAt(1,1));*/
-    }
-    @Test
     public void getRowCount() { assertEquals(0, moveHistory.getRowCount());
     }
     @Test
@@ -34,11 +25,5 @@ public class MoveHistoryTest {
     @Test
     public void getMoves() {
         assert moveHistory.getMoves().isEmpty();
-    }
-    @Test
-    public void addMoves() {
-        moveHistory.addMove("a2-a3");
-        moveHistory.addMove("b7-b6");
-        assertEquals(0, moveHistory.getRowCount());
     }
 }
