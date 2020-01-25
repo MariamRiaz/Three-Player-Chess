9f10d9a122c7f48ff9d4364c12a9b9bd6a0655f7
Stefan Dimitrov <s.dimitrov98@protonmail.ch>
2020-01-25 18:36:39 +0100

    Added missing imports

--- src/main/java/jchess/controller/MoveHistoryController.java
+++ src/main/java/jchess/controller/MoveHistoryController.java
@@ -37,6 +37,8 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 
 /**
