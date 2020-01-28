26334ed941d118a47c922b9d82ae08ba0a8b7d3d
Mariam <mariamriaz51@gmail.com>
2020-01-28 07:15:20 +0100

    Work in progress - Writing tests for RoundChessboard classes

--- src/test/java/jchessTest/RoundChessboardModelTest.java
+++ src/test/java/jchessTest/RoundChessboardModelTest.java
@@ -1,7 +1,14 @@
 package jchessTest;
 
+import jchess.game.GameModel;
+import jchess.game.IGameModel;
+import jchess.game.chessboard.model.RoundChessboardModel;
 import jchess.game.chessboard.model.Square;
+import jchess.game.player.Player;
+import jchess.move.Orientation;
 import jchess.pieces.Piece;
+import jchess.pieces.PieceLoader;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -11,22 +18,33 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class RoundChessboardModelTest {
-    private List<Square> squares = new square1;
+    RoundChessboardModel roundChessboardModel = new RoundChessboardModel(5, 20, false, false);
+    Piece piece;
+    //private List<Square> squares = new square1;
 
-    @Test
-    public void getSquare() {
+    @Before
+    public void setup() {
+        piece = mock(Piece.class);
     }
 
     @Test
-    public void testGetSquare() {
+    public void getSquare()  {
+        assertEquals(null, roundChessboardModel.getSquare(piece));
+        Player testPlayer = new Player("test", "white");
+        piece = new Piece(PieceLoader.getPieceDefinition("King"), testPlayer, new Orientation());
+        assertEquals(null, roundChessboardModel.getSquare(piece));
     }
 
     @Test
     public void getSquaresBetween() {
+        assert roundChessboardModel.getSquaresBetween(null, null).isEmpty();
+
+        roundChessboardModel.getSquaresBetween(new Square(1, 1, null), new Square(1, 5, null));
+        assert roundChessboardModel.getSquaresBetween(null, null).isEmpty();
     }
 
-    @Test
-    public void setPieceOnSquare(Piece, Square) {
-    }
+    //@Test
+   // public void setPieceOnSquare(Piece, Square) {
+  //  }
 
 }
