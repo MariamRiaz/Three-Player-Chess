bdf6eb507dda9865adc62abf9e51ee063bb0b166
Philip Wiegratz <wiegratz.p@gmail.com>
2020-01-28 12:33:25 +0100

    deleted unused stuff

--- src/test/java/jchessTest/game/GameControllerTest.java
+++ src/test/java/jchessTest/game/GameControllerTest.java
@@ -1,33 +1,20 @@
 package jchessTest.game;
 
 import jchess.game.GameController;
-import jchess.game.GameModel;
-import jchess.game.IGameModel;
 import jchess.game.chessboard.controller.IChessboardController;
 import jchess.game.chessboard.controller.RoundChessboardController;
-import jchess.game.player.Player;
-import jchess.io.Images;
 import org.junit.Before;
 import org.junit.Test;
-
-import javax.swing.*;
 
 import static org.mockito.Mockito.*;
 
 public class GameControllerTest {
 
     GameController gameController;
-    Player testPlayer;
-    JOptionPane lul;
-
 
     @Before
     public void setup() {
         gameController = new GameController();
-
-        testPlayer = new Player("", Images.WHITE_COLOR);
-
-        lul = spy(JOptionPane.class);
 
     }
 
