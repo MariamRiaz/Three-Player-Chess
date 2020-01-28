a9bba34f37be597f2f4d9ed0119b899de9e805f2
Philip Wiegratz <wiegratz.p@gmail.com>
2020-01-28 01:51:33 +0100

    gameController done

--- src/main/java/jchess/game/GameController.java
+++ src/main/java/jchess/game/GameController.java
@@ -1,48 +1,27 @@
-/*
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- * Authors:
- * Mateusz Sławomir Lach ( matlak, msl )
- * Damian Marciniak
- */
 package jchess.game;
 
+import jchess.game.chessboard.RoundChessboardLoader;
+import jchess.game.chessboard.controller.IChessboardController;
+import jchess.game.chessboard.controller.RoundChessboardController;
+import jchess.game.chessboard.model.Square;
+import jchess.game.clock.GameClock;
+import jchess.game.clock.IGameClock;
 import jchess.game.history.IMoveHistoryController;
 import jchess.game.history.MoveHistoryController;
 import jchess.game.player.Player;
-import jchess.game.chessboard.controller.RoundChessboardController;
-import jchess.game.clock.GameClock;
-import jchess.game.clock.IGameClock;
-import jchess.game.chessboard.RoundChessboardLoader;
-import jchess.game.chessboard.model.Square;
-import jchess.game.chessboard.controller.IChessboardController;
+import jchess.logging.Log;
 import jchess.move.IMoveEvaluator;
 import jchess.move.MoveEvaluator;
 import jchess.move.buff.BuffEvaluator;
 import jchess.pieces.Piece;
-import jchess.logging.Log;
 
-import javax.swing.*;
 import java.util.HashSet;
 import java.util.Observable;
+import javax.swing.*;
 
 /**
- * Class that represents a chess game. It is responsible for starting and ending games
- * and for storing the currently active player.
- * It also provides functionality for saving and loading games.
+ * Class that represents the Controller of the Game Component,
+ * which includes a Chessboard, MoveHistory and GameClock Component.
  */
 public class GameController implements IGameController {
 
@@ -52,41 +31,54 @@ public class GameController implements IGameController {
     private IChessboardController chessboardController;
     private IMoveHistoryController moveHistoryController;
     private IGameClock gameClock;
-    private final int chessboardSize = 800;
     private RoundChessboardLoader chessboardLoader;
     private IMoveEvaluator moveEvaluator;
 
-
+    /**
+     * Constructor of GameController Class.
+     */
     public GameController() {
 
         gameModel = new GameModel();
         chessboardLoader = new RoundChessboardLoader();
         initializeControllers();
-        gameView = new GameView(this, chessboardController.getView(), moveHistoryController.getScrollPane(), gameClock.getGameClockView());
+        gameView = new GameView(
+                this,
+                chessboardController.getView(),
+                moveHistoryController.getScrollPane(),
+                gameClock.getGameClockView());
         gameModel.setBlockedChessboard(false);
         gameModel.setActivePlayer(gameModel.getPlayerWhite());
     }
 
     private void initializeControllers() {
         moveHistoryController = new MoveHistoryController(chessboardLoader.getColumnNames());
-        chessboardController = new RoundChessboardController(chessboardLoader, chessboardSize, this.gameModel, this.moveHistoryController);
+        int chessboardSize = 800;
+        chessboardController = new RoundChessboardController(
+                chessboardLoader,
+                chessboardSize, this.gameModel,
+                this.moveHistoryController);
         gameClock = new GameClock(this);
         moveEvaluator = new MoveEvaluator((RoundChessboardController) chessboardController);
         chessboardController.addSelectSquareObserver(this);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public IGameModel getGameModel() {
         return gameModel;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public IGameClock getGameClock() {
         return gameClock;
     }
 
     /**
-     * Method to end game
-     *
-     * @param message The message that is shown at the end of the game to each player
+     * {@inheritDoc}
      */
     public void endGame(String message) {
         gameModel.setBlockedChessboard(true);
@@ -94,60 +86,59 @@ public class GameController implements IGameController {
         JOptionPane.showMessageDialog(null, message);
     }
 
-    /**
-     * Method to switch active players after move
-     */
     private void switchActive(boolean forward) {
-        if (forward)
-        	gameModel.setActivePlayer(getNext());
-        else 
-        	gameModel.setActivePlayer(getPrevious());
-        
+        if (forward) {
+            gameModel.setActivePlayer(getNext());
+        } else {
+            gameModel.setActivePlayer(getPrevious());
+        }
+
         this.gameModel.setBlockedChessboard(false);
         this.gameClock.switchPlayers(forward);
         this.moveHistoryController.switchColumns(forward);
     }
-    
+
     private Player getNext() {
-        if (gameModel.getActivePlayer() == gameModel.getPlayerWhite())
+        if (gameModel.getActivePlayer() == gameModel.getPlayerWhite()) {
             return gameModel.getPlayerBlack();
-        if (gameModel.getActivePlayer() == gameModel.getPlayerBlack())
+        }
+        if (gameModel.getActivePlayer() == gameModel.getPlayerBlack()) {
             return gameModel.getPlayerGray();
-        if (gameModel.getActivePlayer() == gameModel.getPlayerGray())
+        }
+        if (gameModel.getActivePlayer() == gameModel.getPlayerGray()) {
             return gameModel.getPlayerWhite();
-        
+        }
         return null;
     }
-    
+
     private Player getPrevious() {
-        if (gameModel.getActivePlayer() == gameModel.getPlayerWhite())
+        if (gameModel.getActivePlayer() == gameModel.getPlayerWhite()) {
             return gameModel.getPlayerGray();
-        if (gameModel.getActivePlayer() == gameModel.getPlayerBlack())
+        }
+        if (gameModel.getActivePlayer() == gameModel.getPlayerBlack()) {
             return gameModel.getPlayerWhite();
-        if (gameModel.getActivePlayer() == gameModel.getPlayerGray())
+        }
+        if (gameModel.getActivePlayer() == gameModel.getPlayerGray()) {
             return gameModel.getPlayerBlack();
-        
+        }
         return null;
     }
 
     /**
-     * Checks if the current state can be undone
-     *
-     * @return True if the current state of the game is undoable
+     * {@inheritDoc}
      */
     public boolean undo() {
         boolean status;
 
         status = chessboardController.undo();
-        if (status)
+        if (status) {
             this.switchActive(false);
+        }
         return status;
     }
 
     /**
-     * Checks if the current game can be rewound to the start
-     *
-     * @return True if the game can be rewound
+     * {@inheritDoc}
      */
     public boolean rewindToBegin() {
         boolean result = false;
@@ -159,10 +150,7 @@ public class GameController implements IGameController {
     }
 
     /**
-     * Checks if the current game can be rewound to the end
-     *
-     * @return Returns true if the current game can be rewound to the end
-     * @throws UnsupportedOperationException When the game cannot be rewound
+     * {@inheritDoc}
      */
     public boolean rewindToEnd() throws UnsupportedOperationException {
         boolean result = false;
@@ -174,32 +162,30 @@ public class GameController implements IGameController {
     }
 
     /**
-     * Checks if a move can be redone from the current state of the game (undo an undo)
-     *
-     * @return True if the most recent undone move can be redone
+     * {@inheritDoc}
      */
     public boolean redo() {
         boolean status = chessboardController.redo();
-        if (status)
+        if (status) {
             switchActive(true);
+        }
 
         return status;
     }
 
-    /**
-     * Includes logic that happens every time a player selects a square
-     *
-     * @param square The selected square
-     */
     private void selectedSquare(Square square) {
-        if (gameModel.isBlockedChessboard()) return;
-        if (square == null) return;
+        if (gameModel.isBlockedChessboard()) {
+            return;
+        }
+        if (square == null) {
+            return;
+        }
         if (chessboardController.getActiveSquare() != null) {
             if (didSelectPossibleMove(square)) {
                 chessboardController.move(chessboardController.getActiveSquare(), square, true, true);
                 chessboardController.unselect();
                 applyBuffs();
-                
+
                 switchActive(true);
                 if (isCheckMate()) {
                     this.endGame("Checkmate! " + gameModel.getActivePlayer().getName() + " player won!");
@@ -214,7 +200,11 @@ public class GameController implements IGameController {
     }
 
     private void applyBuffs() {
-        BuffEvaluator evaluator = new BuffEvaluator(chessboardController, moveHistoryController, moveEvaluator, getNext());
+        BuffEvaluator evaluator = new BuffEvaluator(
+                chessboardController,
+                moveHistoryController,
+                moveEvaluator,
+                getNext());
         evaluator.evaluate();
     }
 
@@ -230,25 +220,23 @@ public class GameController implements IGameController {
         return square.getPiece().getPlayer().equals(gameModel.getActivePlayer());
     }
 
-    private boolean didSelectSameSquare(Square square) {
-        return square.equals(chessboardController.getActiveSquare());
-    }
-
     private boolean didSelectPossibleMove(Square square) {
         return chessboardController.moveIsPossible(chessboardController.getActiveSquare(), square);
     }
 
     /**
-     * Listens for events that come every time a square is selected
-     *
-     * @param o   The observable square that generates the events
-     * @param arg The new generated Square
+     * {@inheritDoc}
      */
     public void update(Observable o, Object arg) {
         Square square = (Square) arg;
         selectedSquare(square);
     }
 
+    /**
+     * getter for the View of the Game Component.
+     *
+     * @return View of the Game Component
+     */
     public AbstractGameView getView() {
         return this.gameView;
     }
--- src/main/java/jchess/game/IGameController.java
+++ src/main/java/jchess/game/IGameController.java
@@ -4,22 +4,66 @@ import jchess.game.clock.IGameClock;
 
 import java.util.Observer;
 
+/**
+ * Interface that a GameController must implement.
+ */
 public interface IGameController extends Observer {
 
+    /**
+     * getter for Model of the Game Component.
+     *
+     * @return GameModel
+     */
     IGameModel getGameModel();
 
+    /**
+     * getter for the Game Clock Component.
+     *
+     * @return GameClock
+     */
     IGameClock getGameClock();
 
+    /**
+     * getter for the View of the Game Component.
+     *
+     * @return View of the Game Component
+     */
     AbstractGameView getView();
 
+    /**
+     * Method to end the Game and show Message on Game End.
+     *
+     * @param message Message
+     */
     void endGame(String message);
 
+
+    /**
+     * Checks if the current state can be undone.
+     *
+     * @return True if the current state of the game is undoable
+     */
     boolean undo();
 
+    /**
+     * Checks if a move can be redone from the current state of the game (undo an undo).
+     *
+     * @return True if the most recent undone move can be redone
+     */
     boolean redo();
 
+    /**
+     * Undoes all Moves to the Begin.
+     *
+     * @return Returns true if the current game can be rewound to the begin
+     */
     boolean rewindToBegin();
 
+    /**
+     * Undoes all Moves that were undone.
+     *
+     * @return Returns true if the current game can be rewound to the end
+     */
     boolean rewindToEnd();
 
 }
