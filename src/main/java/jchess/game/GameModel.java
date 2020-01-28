23055aad809e53a2befbb3e3ea1395c2f68f8539
Philip Wiegratz <wiegratz.p@gmail.com>
2020-01-28 02:00:43 +0100

    done with game model

--- src/main/java/jchess/game/GameModel.java
+++ src/main/java/jchess/game/GameModel.java
@@ -23,11 +23,8 @@ package jchess.game;
 import jchess.game.player.Player;
 import jchess.io.Images;
 
-import java.util.PropertyResourceBundle;
-import java.util.ResourceBundle;
-
 /**
- * Class representings game gameModel available for the current player
+ * Class representings game gameModel available for the current player.
  */
 public class GameModel implements IGameModel {
 
@@ -40,46 +37,79 @@ public class GameModel implements IGameModel {
     private Player playerBlack;
     private Player playerGray;
 
+    /**
+     * {@inheritDoc}
+     */
     public void setTimeForGame(int timeForGame) {
         this.timeForGame = timeForGame;
     }
 
-    public boolean getTimeLimitSet(){
+    /**
+     * {@inheritDoc}
+     */
+    public boolean getTimeLimitSet() {
         return timeLimitSet;
     }
 
-    public void setTimeLimitSet(boolean limitSet){
+    /**
+     * {@inheritDoc}
+     */
+    public void setTimeLimitSet(boolean limitSet) {
         this.timeLimitSet = limitSet;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public Player getPlayerWhite() {
         return playerWhite;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public Player getPlayerBlack() {
         return playerBlack;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public Player getPlayerGray() {
         return playerGray;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void setBlockedChessboard(boolean blockedChessboard) {
         this.blockedChessboard = blockedChessboard;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public boolean isBlockedChessboard() {
         return blockedChessboard;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void setActivePlayer(Player activePlayer) {
         this.activePlayer = activePlayer;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public Player getActivePlayer() {
         return activePlayer;
     }
 
+    /**
+     * Constructor for GameModel Class.
+     */
     public GameModel() {
         this.playerWhite = new Player("", Images.WHITE_COLOR);
         this.playerBlack = new Player("", Images.BLACK_COLOR);
@@ -88,9 +118,7 @@ public class GameModel implements IGameModel {
     }
 
     /**
-     * Method to get game time set by player
-     *
-     * @return timeFofGame int with how long the game will leasts
+     * {@inheritDoc}
      */
     public int getTimeForGame() {
         return this.timeForGame;
